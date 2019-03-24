8e696416b469068d83c1a44b3db4dbfdad0be3ec
刘家豪 <37140960+2701150117@users.noreply.github.com>
2019-03-24 10:58:46 +0800

    初始化项目

---
+++ src/main/java/com/liu/haoahstu/HaoAhstuApplication.java
@@ -0,0 +1,13 @@
+package com.liu.haoahstu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaoAhstuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaoAhstuApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/liu/haoahstu/constants/VarConstants.java
@@ -0,0 +1,14 @@
+package com.liu.haoahstu.constants;
+
+/**
+ * @author huaisun
+ * @date 2019/2/23 17:58
+ */
+public class VarConstants {
+
+    /**
+     * 启用，停用
+     */
+    public static final Integer START = 1;
+    public static final Integer STOP = 0;
+}
